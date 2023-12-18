37d75fea167ce2f6525bdb4ef28b72b600653b06
Isaac Olajos <isaacolajos@hotmail.com>
2023-12-18 09:47:27 -0500

    assets

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+$(window).scroll(function() {    
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 50) {
+      $("header").addClass("nav-fixed");
+  } else {
+      $("header").removeClass("nav-fixed");
+  }
+});
+
+$('.navbar .nav-item .nav-link').on('click', function(){
+  $('.navbar .nav-item .nav-link').removeClass('active');
+  $(this).addClass('active');
+});
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+$('section [class*=-item]').addClass('reveal');
+  
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
+  
+
+  
+  
+  
+  
+  
